2fea25205d2bf93175bfb6c76f23f3ba9500f26a
Vsvvn <Vladsun@yandex.ru>
2022-12-19 06:36:31 +0300

    Сделано дз.

---
+++ src/transport/driver/Driver.java
@@ -0,0 +1,48 @@
+package transport.driver;
+//Задание 2
+
+import transport.Transport;
+
+public abstract class Driver<T extends Transport> {
+
+    private final String name;
+    private boolean hasDrivingLicense;
+    private int experience;
+
+    public Driver(String name, boolean hasDrivingLicense, int experience) {
+        this.name = name;
+        this.hasDrivingLicense = hasDrivingLicense;
+        this.experience = experience;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isHasDrivingLicense() {
+        return hasDrivingLicense;
+    }
+
+    public void setHasDrivingLicense(boolean hasDrivingLicense) {
+        this.hasDrivingLicense = hasDrivingLicense;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public void setExperience(int experience) {
+        this.experience = experience;
+    }
+
+    public abstract void startMove(T transport);
+
+    public abstract void stopMove(T transport);
+
+    public abstract void refill(T transport);
+
+    public void driverInfo(T transport) {
+        System.out.println("водитель " + name + " управляет " + transport.getBrand());
+        transport.printType();
+    }
+}
---
+++ src/transport/driver/DriverB.java
@@ -0,0 +1,27 @@
+package transport.driver;
+
+
+import transport.Car;
+
+public class DriverB extends Driver<Car> {
+
+    public DriverB(String name, boolean hasDrivingLicense, int experience) {
+        super(name, hasDrivingLicense, experience);
+    }
+
+    @Override
+    public void startMove(Car transport) {
+        transport.start();
+    }
+
+    @Override
+    public void stopMove(Car transport) {
+        transport.finish();
+    }
+
+    @Override
+    public void refill(Car transport) {
+        System.out.println("Водитель"+ getName() + " заправляет легковой автомобиль " + transport.getBrand());
+
+    }
+}
